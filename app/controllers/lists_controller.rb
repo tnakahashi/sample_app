70d7f078797f8e134457dd6bcb75e498ca7cacf4
TakumaNakahashi <115599704+tnakahashi@users.noreply.github.com>
2022-11-11 14:06:50 +0000

    [Finish]Ch04完了

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,12 +5,10 @@ class ListsController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)  
   end
 
   def index
@@ -18,6 +16,7 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])  
   end
 
   def edit
